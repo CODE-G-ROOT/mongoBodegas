c03f06a1d2398b349e8dc36c0a50c4b34a8f9109
JACKode <123758906+JuanDavidAvilaRaveloCampus@users.noreply.github.com>
2023-08-24 07:22:53 -0500

    feat: :sparkles: DTO Historiales
    implementación del dto de historiales, no se validaron las fehcas

--- backend/source/historiales.ts
+++ backend/source/historiales.ts
@@ -1,1 +1,18 @@
-..
+import { Expose, Transform } from 'class-transformer';
+import { IsDate, IsDefined, IsNumber, IsString, Matches} from 'class-validator';
+
+export class Historiales {
+    @Expose({ name: "id"})
+    @IsNumber({}, { message: () => { throw { status: 400, message: "id param must be a Number type", reference: "https://http.cat/400" } } })
+    @IsDefined({ message: () =>  { throw { status: 400, message: "id the param is required", reference: "https://http.cat/400"}}})
+    id_historiales: number = 0;
+
+    @Expose({ name: "name"})
+    @IsNumber({}, { message: () => { throw { status: 400, message: "name must be a string type", reference: "https://http.cat/400"}}})
+    @IsDefined({ message: () => { throw { status: 400, message: "name param is required"}}})
+    cantidad: number = 0;
+
+    constructor(data: Partial<Historiales>) {
+        Object.assign(this, data);
+    }
+}
